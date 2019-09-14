3c4c904fe9be8dd7e328fc50a1cb6432cccc5317
Priscila Rachevsky <i868268@slel00681879b.amer.global.corp.sap>
2019-09-14 22:07:29 +0200

    initial

---
+++ Maracana1.py
@@ -0,0 +1,62 @@
+# maracana1:
+# origin-point (goal floor-bottom):  X=124px Y=157px
+# pixels=(0,0)
+# meters=(0,0,0)
+
+# point 1 (goal floor-top): X=160px Y=138px
+# pixels=(36,19)
+# meters=(0,7.32,0)
+u1 = 36
+v1 = 19
+x1 = 0
+y1 = 7.32
+z1 = 0
+
+# point 2 (goal ceil-top): X=159px Y=110px
+# pixels=(35,47)
+# meters=(0,7.32,2.44)
+u2 = 36
+v2 = 47
+x2 = 0
+y2 = 7.32
+z2 = 2.44
+
+# point 3 (goal ceil-top): X=123px Y=126px
+# pixels=(-1,31)
+# meters=(0,0,2.44)
+u3 = -1
+v3 = 31
+x3 = 0
+y3 = 0
+z3 = 2.44
+
+# point 4 (top-right litte-area): X=241px Y=130px
+# pixels=(177,27)
+# meters=(5.5,12.82,0)
+u4 = 177
+v4 = 27
+x4 = 5.5
+y4 = 12.82
+z4 = 0
+
+# point 5 (botton-right litte-area): X=160px Y=175px
+# pixels=(36,-18)
+# meters=(5.5,-5.5,0)
+u5 = 36
+v5 = -18
+x5 = 5.5
+y5 = -5.5
+z5 = 0
+
+# point 6 (botton-left little-area): X=97px Y=170px
+# pixels=(27,-13)
+# meters=(0,-5.5,0)
+u6 = 27
+v6 = -13
+x6 = 0
+y6 = -5.5
+z6 = 0
+
+# point 7 (top-left little-area): X=184px Y=126px
+# pixels=(60,31)
+# meters=(0,12.82,0)
---
+++ Maracana2.py
@@ -0,0 +1,58 @@
+# maracana2:
+# origin-point (penalti circle):  X=377px Y=134px
+# pixels=(0,0)
+# meters=(0,0,0)
+
+# point 1 (top-right little-area): X=575px Y=99px
+# pixels=(198,-40)
+# meters=(11,-9.16, 0)
+u1 = 198
+v1 = -40
+x1 = 11
+y1 = -9.16
+z1 = 0
+
+# point 2 (botton-left little-area): X=509px Y=178px
+# pixels=(132,44)
+# meters=(5.5,9.16,0)
+u2 = 132
+v2 = 44
+x2 = 5.5
+y2 = 9.16
+z2 = 0
+
+# point 3 (top-left little-area): X=473px Y=99px
+# pixels=(96, -35)
+# meters=(5.5,-9.16,0)
+u3 = 96
+v3 = -35
+x3 = 5.5
+y3 = -9.16
+z3 = 0
+
+# point 4 (top-left big-area): X=268px Y=61px
+# pixels=(-109,-73)
+# meters=(-5.5,-20.16,0)
+u4 = -109
+v4 = -73
+x4 = -5.5
+y4 = -20.16
+z4 = 0
+
+# point 5 (botton-left big-area):
+# pixels=(-111,103)
+# meters=(-5.5,20.16,0)
+u5 = -111
+v5 = 103
+x5 = -5.5
+y5 = 20.16
+z5 = 0
+
+# point 6 (top-right big-area): X=520px Y=61px
+# pixels=(143,-73)
+# meters=(11,-9.16,0)
+u6 = 143
+v6 = -73
+x6 = 11
+y6 = -9.16
+z6 = 0
