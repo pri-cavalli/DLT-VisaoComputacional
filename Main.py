8dde209968dab01832e7bb9c58443b0371b65bcc
Felipe Leivas <felipe.leivas@sap.com>
2019-09-17 17:09:03 -0300

    Added method to convert from 2D to 3D

--- Main.py
+++ Main.py
@@ -1,11 +1,6 @@
-import sys
 import numpy as np
-from numpy.linalg import svd
-
-import Maracana1 as points
-from sympy import *
-
-import cv2
+from numpy.linalg import svd, inv
+from Maracana1 import *
 
 
 def calculatePixels(x, y, z, cameraMatrix):
@@ -23,83 +18,53 @@ def calculatePixels(x, y, z, cameraMatrix):
     m34 = cameraMatrix[2][3]
     u = (m11 * x + m12 * y + m13 * z + m14) / (m31 * x + m32 * y + m33 * z + m34)
     v = (m21 * x + m22 * y + m23 * z + m24) / (m31 * x + m32 * y + m33 * z + m34)
-    return u, v
+    return u + 124, v + 157
+
+
+def calculateRealWorldPoint(point, cameraMatrix):
+    u = point[0]
+    v = point[1]
+    m11 = cameraMatrix[0][0]
+    m12 = cameraMatrix[0][1]
+    m14 = cameraMatrix[0][3]
+    m21 = cameraMatrix[1][0]
+    m22 = cameraMatrix[1][1]
+    m24 = cameraMatrix[1][3]
+    m31 = cameraMatrix[2][0]
+    m32 = cameraMatrix[2][1]
+    m34 = cameraMatrix[2][3]
+    inverseHomography = inv(np.array([np.array([m11, m12, m14]), np.array([m21, m22, m24]), np.array([m31, m32, m34])]))
+    worldCordinates = inverseHomography.dot(np.array([u, v, 1]))
+
+    realWorldX = worldCordinates[0] / worldCordinates[2]
+    realWorldY = worldCordinates[1] / worldCordinates[2]
+
+    return [realWorldX, realWorldY, 0]
 
 
 def main():
-    x1 = points.x1
-    x2 = points.x2
-    x3 = points.x3
-    x4 = points.x4
-    x5 = points.x5
-    x6 = points.x6
-
-    y1 = points.y1
-    y2 = points.y2
-    y3 = points.y3
-    y4 = points.y4
-    y5 = points.y5
-    y6 = points.y6
-
-    z1 = points.z1
-    z2 = points.z2
-    z3 = points.z3
-    z4 = points.z4
-    z5 = points.z5
-    z6 = points.z6
-
-    u1 = points.u1
-    u2 = points.u2
-    u3 = points.u3
-    u4 = points.u4
-    u5 = points.u5
-    u6 = points.u6
-
-    v1 = points.v1
-    v2 = points.v2
-    v3 = points.v3
-    v4 = points.v4
-    v5 = points.v5
-    v6 = points.v6
-
     A = np.array([
         [x1, y1, z1, 1, 0, 0, 0, 0, -u1 * x1, -u1 * y1, -u1 * z1, -u1],
         [0, 0, 0, 0, x1, y1, z1, 1, -v1 * x1, -v1 * y1, -v1 * z1, -v1],
-        [x2, y2, z2, 2, 0, 0, 0, 0, -u2 * x2, -u2 * y2, -u2 * z2, -u2],
-        [0, 0, 0, 0, x2, y2, z2, 2, -v2 * x2, -v2 * y2, -v2 * z2, -v2],
-        [x3, y3, z3, 3, 0, 0, 0, 0, -u3 * x3, -u3 * y3, -u3 * z3, -u3],
-        [0, 0, 0, 0, x3, y3, z3, 3, -v3 * x3, -v3 * y3, -v3 * z3, -v3],
-        [x4, y4, z4, 4, 0, 0, 0, 0, -u4 * x4, -u4 * y4, -u4 * z4, -u4],
-        [0, 0, 0, 0, x4, y4, z4, 4, -v4 * x4, -v4 * y4, -v4 * z4, -v4],
-        [x5, y5, z5, 5, 0, 0, 0, 0, -u5 * x5, -u5 * y5, -u5 * z5, -u5],
-        [0, 0, 0, 0, x5, y5, z5, 5, -v5 * x5, -v5 * y5, -v5 * z5, -v5],
-        [x6, y6, z6, 6, 0, 0, 0, 0, -u6 * x6, -u6 * y6, -u6 * z6, -u6],
-        [0, 0, 0, 0, x6, y6, z6, 6, -v6 * x6, -v6 * y6, -v6 * z6, -v6]
+        [x2, y2, z2, 1, 0, 0, 0, 0, -u2 * x2, -u2 * y2, -u2 * z2, -u2],
+        [0, 0, 0, 0, x2, y2, z2, 1, -v2 * x2, -v2 * y2, -v2 * z2, -v2],
+        [x3, y3, z3, 1, 0, 0, 0, 0, -u3 * x3, -u3 * y3, -u3 * z3, -u3],
+        [0, 0, 0, 0, x3, y3, z3, 1, -v3 * x3, -v3 * y3, -v3 * z3, -v3],
+        [x4, y4, z4, 1, 0, 0, 0, 0, -u4 * x4, -u4 * y4, -u4 * z4, -u4],
+        [0, 0, 0, 0, x4, y4, z4, 1, -v4 * x4, -v4 * y4, -v4 * z4, -v4],
+        [x5, y5, z5, 1, 0, 0, 0, 0, -u5 * x5, -u5 * y5, -u5 * z5, -u5],
+        [0, 0, 0, 0, x5, y5, z5, 1, -v5 * x5, -v5 * y5, -v5 * z5, -v5],
+        [x6, y6, z6, 1, 0, 0, 0, 0, -u6 * x6, -u6 * y6, -u6 * z6, -u6],
+        [0, 0, 0, 0, x6, y6, z6, 1, -v6 * x6, -v6 * y6, -v6 * z6, -v6]
     ])
     U, S, V = svd(A)
 
-    P = V[11].reshape(3, 4)
+    P = V[len(V) - 1].reshape(3, 4)
 
-    m11 = P[0][0]
-    m12 = P[0][1]
-    m13 = P[0][2]
-    m14 = P[0][3]
-    m21 = P[1][0]
-    m22 = P[1][1]
-    m23 = P[1][2]
-    m24 = P[1][3]
-    m31 = P[2][0]
-    m32 = P[2][1]
-    m33 = P[2][2]
-    m34 = P[2][3]
-
-    x = x1
-    y = y1
-    z = z1
-    u = (m11 * x + m12 * y + m13 * z + m14) / (m31 * x + m32 * y + m33 * z + m34)
-    v = (m21 * x + m22 * y + m23 * z + m24) / (m31 * x + m32 * y + m33 * z + m34)
-    print(u)
-
+    givenPoint = np.array([159, 110])
+    givenPoint = givenPoint - np.array([origenX, origenY])
+    footRealWorldCords = calculateRealWorldPoint(givenPoint, P)
+    headRealWorldCords = [footRealWorldCords[0], footRealWorldCords[1], 1.8]
 
 if __name__ == '__main__':
     main()
--- Maracana1.py
+++ Maracana1.py
@@ -1,13 +1,16 @@
 # maracana1:
 # origin-point (goal floor-bottom):  X=124px Y=157px
+origenX = 124
+origenY = 157
+
 # pixels=(0,0)
 # meters=(0,0,0)
 
 # point 1 (goal floor-top): X=160px Y=138px
 # pixels=(36,19)
 # meters=(0,7.32,0)
-u1 = 36
-v1 = 19
+u1 = 160 - origenX
+v1 = 138 - origenY
 x1 = 0
 y1 = 7.32
 z1 = 0
@@ -15,8 +18,8 @@ z1 = 0
 # point 2 (goal ceil-top): X=159px Y=110px
 # pixels=(35,47)
 # meters=(0,7.32,2.44)
-u2 = 35
-v2 = 47
+u2 = 159 - origenX
+v2 = 110 - origenY
 x2 = 0
 y2 = 7.32
 z2 = 2.44
@@ -24,8 +27,8 @@ z2 = 2.44
 # point 3 (goal ceil-top): X=123px Y=126px
 # pixels=(-1,31)
 # meters=(0,0,2.44)
-u3 = -1
-v3 = 31
+u3 = 123 - origenX
+v3 = 126 - origenY
 x3 = 0
 y3 = 0
 z3 = 2.44
@@ -33,8 +36,8 @@ z3 = 2.44
 # point 4 (top-right litte-area): X=241px Y=130px
 # pixels=(177,27)
 # meters=(5.5,12.82,0)
-u4 = 177
-v4 = 27
+u4 = 241 - origenX
+v4 = 130 - origenY
 x4 = 5.5
 y4 = 12.82
 z4 = 0
@@ -42,8 +45,8 @@ z4 = 0
 # point 5 (botton-right litte-area): X=160px Y=175px
 # pixels=(36,-18)
 # meters=(5.5,-5.5,0)
-u5 = 36
-v5 = -18
+u5 = 160 - origenX
+v5 = 175 - origenY
 x5 = 5.5
 y5 = -5.5
 z5 = 0
@@ -51,8 +54,8 @@ z5 = 0
 # point 6 (botton-left little-area): X=97px Y=170px
 # pixels=(27,-13)
 # meters=(0,-5.5,0)
-u6 = 27
-v6 = -13
+u6 = 97 - origenX
+v6 = 170 - origenY
 x6 = 0
 y6 = -5.5
 z6 = 0
@@ -60,3 +63,17 @@ z6 = 0
 # point 7 (top-left little-area): X=184px Y=126px
 # pixels=(60,31)
 # meters=(0,12.82,0)
+u7 = 184 - origenX
+v7 = 126 - origenY
+x7 = 0
+y7 = 12.82
+z7 = 0
+
+# point 8 (top-left big-area): X=366px Y=131px
+# pixels=(242, 26)
+# meters=(0,23.82,0)
+u8 = 366 - origenX
+v8 = 131 - origenY
+x8 = 0
+y8 = 23.82
+z8 = 0
